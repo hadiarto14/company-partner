f98ff2a7b0d053bc623e6d3b441e20059cce0925
Hadiarto <hadiarto_banjarnahor@bca.co.id>
2022-11-10 12:49:27 +0700

    [Add] Confirmation after submit

--- src/app/partner-add/partner-add.component.ts
+++ src/app/partner-add/partner-add.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Partner } from '../model/partner';
 import { PartnerService } from '../service/partner.service';
+import { ConfirmationComponent } from '../confirmation/confirmation.component';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-partner-add',
@@ -11,7 +14,8 @@ import { PartnerService } from '../service/partner.service';
 export class PartnerAddComponent implements OnInit {
   partnerForm: FormGroup;
   submitted = false;
-  constructor(public fb: FormBuilder, private partnerService:PartnerService) { 
+  
+  constructor(public fb: FormBuilder, private partnerService:PartnerService, private dialog: MatDialog, private router: Router) { 
     this.partnerForm = this.fb.group({
       'name' : fb.control('', (Validators.required)),
       'skill' : fb.control('', (Validators.required)),
@@ -40,11 +44,21 @@ export class PartnerAddComponent implements OnInit {
   submit(){
     this.validateForm()
     this.submitted = true
+    const dialogRef = this.dialog.open(ConfirmationComponent, {
+      data: {
+        message: 'Do you want to Add another partner?'
+      }
+    });
     if(this.partnerForm.valid){
       this.partnerService.addPartner(this.partnerForm.getRawValue() as Partner)
       .subscribe(partner => {
-        this.partnerForm.reset();
-        
+        dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+          if (confirmed) {
+            this.partnerForm.reset();
+          }else{
+            this.router.navigate([''])
+          }
+        });
       })
     }
   }
--- src/app/partner-content/partner-content.component.ts
+++ src/app/partner-content/partner-content.component.ts
@@ -18,33 +18,23 @@ export class PartnerContentComponent implements OnInit {
   }
 
   remove(id: number) {
-    // swal({
-    //   title: 'Warning',
-    //   text: 'Are you sure wanna delete All Training History?',
-    //   icon: 'warning',
-    //   buttons: ['Cancel', 'Confirm'],
-    //   dangerMode: true
-    // }).then(confirm => {
-    //   if(confirm){
-        let index = this.partners?.findIndex(i => i.id === id)
-        this.partnerService.deletePartner(id).subscribe(a => this.partners.splice(index, 1))
-      // }
-    // })
+    let index = this.partners?.findIndex(i => i.id === id)
+    this.partnerService.deletePartner(id).subscribe(a => this.partners.splice(index, 1))
 
   }
 
-  openDialog(id:number) {
-		const dialogRef = this.dialog.open(ConfirmationComponent,{
-		data:{
-			message: 'Do you want to delete the partner from list?'
-		}
-		});
-		
-		dialogRef.afterClosed().subscribe((confirmed: boolean) => {
-			if (confirmed) {
-				this.remove(id);
-			}
-		});
-	} 
+  openDialog(id: number) {
+    const dialogRef = this.dialog.open(ConfirmationComponent, {
+      data: {
+        message: 'Do you want to delete the partner from list?'
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+      if (confirmed) {
+        this.remove(id);
+      }
+    });
+  }
 
 }
